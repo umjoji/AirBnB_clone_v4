d1b97046ee26c2a2fbe29e1044e02bd8bd8ca240
umjoji <30466162+umjoji@users.noreply.github.com>
2023-05-07 00:03:56 +0100

    Add files and README

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from flasgger.utils import swag_from
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
@@ -46,4 +46,4 @@ if __name__ == "__main__":
         host = '0.0.0.0'
     if not port:
         port = '5000'
-    app.run(host=host, port=port, threaded=True)
+    app.run(host=host, port=port, threaded=True) # type: ignore
